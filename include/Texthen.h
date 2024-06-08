9b53dd695ab16f9c4a932e3d9be95179faefe71a
Shalnov.Yuri.Pipr2.3Kurs <96010611+jra1251@users.noreply.github.com>
2024-06-09 02:04:17 +0300

    Add files via upload

---
+++ include/Texthen.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <map>
+#include <fstream>
+#define rand_r rand
+
+const int INITIAL_PREFIX_SIZE = 2;
+const int GENERATION_LIMIT = 1000;
+
+class TextGenerator {
+private:
+    int currentPrefixLength;
+    typedef std::deque<std::string> prefix;
+    std::map<prefix, std::vector<std::string> > stateTable;
+    std::vector<std::string> wordPool;
+public:
+    TextGenerator(std::string InputFile, int preflen);
+    std::string getText(int wordPoolamount);
+    int get—urrentPrefixLength();
+    std::string getSuffix(std::deque<std::string> prefdeq);
+    void WriteOnFileGenerateText(int wordPoolamount);
+};
