4455327d5b2c609cfdec1f60f30c116b90dd354e
Shalnov.Yuri.Pipr2.3Kurs <96010611+jra1251@users.noreply.github.com>
2024-06-09 02:13:53 +0300

    Update Markov_1.cpp

--- src/Markov_1.cpp
+++ src/Markov_1.cpp
@@ -10,7 +10,6 @@ const int MAXGEN = 1000;
 
 int main() {
     setlocale(LC_ALL, "rus");
-    
     std::string File_name = "input_file.txt";
     TextGenerator  TGenerate = TextGenerator(File_name, 2);
     std::cout << TGenerate.getText(MAXGEN) << std::endl;
