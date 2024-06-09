bbfc936524cc336b756e30d873f5118466812693
Shalnov.Yuri.Pipr2.3Kurs <96010611+jra1251@users.noreply.github.com>
2024-06-09 03:02:30 +0300

    s

--- test/tests.cpp
+++ test/tests.cpp
@@ -3,38 +3,38 @@
 
 #include "textgen.h"
 TEST(task1, test1) {
-    TextGenerator Tg = TextGenerator("test.txt", NPREF);
-    int result = Tg.getPrefixSize();
+    TextGenerator Tg = TextGenerator("test.txt", 2);
+    int result = Tg.get2ixSize();
     EXPECT_EQ(2, result);
 }
 
 TEST(task2, test2) {
-    TextGenerator Tg = TextGenerator("test.txt", NPREF);
-    std::deque<std::string> newPrefixes{"rock", "spewed"};
-    std::string newSuffix = Tg.getSuffix(newPrefixes);
+    TextGenerator Tg = TextGenerator("test.txt", 2);
+    std::deque<std::string> new2ixes{"rock", "spewed"};
+    std::string newSuffix = Tg.getSuffix(new2ixes);
     EXPECT_EQ("forth", newSuffix);
 }
 
 TEST(task3, test3) {
-    TextGenerator  Tg = TextGenerator("test.txt", NPREF);
-    std::deque<std::string> newPrefixes {"corpses", "buried"};
-    std::string newSuffix = Tg.getSuffix(newPrefixes);
+    TextGenerator  Tg = TextGenerator("test.txt", 2);
+    std::deque<std::string> new2ixes {"corpses", "buried"};
+    std::string newSuffix = Tg.getSuffix(new2ixes);
     EXPECT_EQ("for", newSuffix);
 }
 
 TEST(task4, test4) {
-    TextGenerator Tg = TextGenerator("test.txt", NPREF);
-    std::deque<std::string> newPrefixes {"things", "worth"};
-    std::string newSuffix = Tg.getSuffix(newPrefixes);
+    TextGenerator Tg = TextGenerator("test.txt", 2);
+    std::deque<std::string> new2ixes {"things", "worth"};
+    std::string newSuffix = Tg.getSuffix(new2ixes);
     EXPECT_TRUE(newSuffix == "dying" || newSuffix == "living");
 }
 
 TEST(task5, test5) {
-    TextGenerator Tg = TextGenerator("test.txt", NPREF);
-    std::string text = Tg.getText(MAXGEN);
+    TextGenerator Tg = TextGenerator("test.txt", 2);
+    std::string text = Tg.getText(1000);
     int counter = 0;
     for (int i = 0; i < text.size(); i++) {
         if (text[i] == ' ') counter++;
     }
-    EXPECT_LE(counter, MAXGEN + 1);
+    EXPECT_LE(counter, 1000 + 1);
 }
